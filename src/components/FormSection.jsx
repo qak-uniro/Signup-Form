ed708cd851edb4ebd669393b08e6547505e52700
unknown <qakasun3@gmail.com>
2025-02-06 10:01:46 +0530

    form update validations:modified again

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -4,19 +4,19 @@ import { useFormik } from "formik";
 const validate = (values) => {
   const errors = {};
   if (!values.firstName) {
-    errors.firstName = "First Name cannot be empty";
+    errors.firstName = "FirstName cannot be empty";
   } else if (values.firstName.length > 15) {
     errors.firstName = "Must be 20 characters or less";
   }
 
   if (!values.lastName) {
-    errors.lastName = "Last Name cannot be empty";
+    errors.lastName = "LastName cannot be empty";
   } else if (values.lastName.length > 20) {
     errors.lastName = "Must be 20 characters or less";
   }
 
   if (!values.email) {
-    errors.email = "Email is required";
+    errors.email = "E-mail is required";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = "Invalid E-mail address";
   }
