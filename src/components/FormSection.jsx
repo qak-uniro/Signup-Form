8b7935ff2befdfffd22eecf2c554f1d996f34ff2
unknown <qakasun3@gmail.com>
2025-02-06 09:42:52 +0530

    form update validations:modified

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -6,19 +6,19 @@ const validate = (values) => {
   if (!values.firstName) {
     errors.firstName = "First Name cannot be empty";
   } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
+    errors.firstName = "Must be 20 characters or less";
   }
 
   if (!values.lastName) {
     errors.lastName = "Last Name cannot be empty";
   } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 25 characters or less";
+    errors.lastName = "Must be 20 characters or less";
   }
 
   if (!values.email) {
     errors.email = "Email is required";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid Email address";
+    errors.email = "Invalid E-mail address";
   }
 
   if (!values.password) {
