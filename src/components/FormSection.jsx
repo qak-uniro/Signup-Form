8c4a1377a7dc4cbe586252a0083b62a0d4adfcee
unknown <qakasun3@gmail.com>
2025-02-06 11:13:56 +0530

    validation error message modification

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -5,20 +5,20 @@ const validate = (values) => {
   const errors = {};
   if (!values.firstName) {
     errors.firstName = "FirstName cannot be empty";
-  } else if (values.firstName.length > 15) {
+  } else if (values.firstName.length > 55) {
     errors.firstName = "Must be 25 characters or less";
   }
 
   if (!values.lastName) {
     errors.lastName = "LastName cannot be empty";
-  } else if (values.lastName.length > 20) {
+  } else if (values.lastName.length > 25) {
     errors.lastName = "Must be 25 characters or less";
   }
 
   if (!values.email) {
-    errors.email = "Email is required";
+    errors.email = "E-mail is required";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid Email address";
+    errors.email = "Invalid E-mail address";
   }
 
   if (!values.password) {
